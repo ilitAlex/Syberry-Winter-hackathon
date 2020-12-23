4ca357764746422131dce071f52d9163fcdf0414
victorchilari <victor1chilari1@gmail.com>
2020-12-23 10:09:58 +0200

    [SH-006] Product page without styles

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import MainPage from "./pages/MainPage/MainPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
 export const useRoutes = (isAuthenticated) => {
   if (isAuthenticated) {
     return (
       <Switch>
-        {/* <Route path="/product/:id" component={ProductPage} />
+        {/*
         <Route path="/user/:id">
           <UserPage />
         </Route>*/}
+        <Route path="/product/:id" component={ProductPage} />
         <Route path="/auth" exact>
           <AuthPage />
         </Route>
@@ -24,6 +26,7 @@ export const useRoutes = (isAuthenticated) => {
 
   return (
     <Switch>
+      <Route path="/product/:id" component={ProductPage} />
       <Route path="/auth" exact>
         <AuthPage />
       </Route>
