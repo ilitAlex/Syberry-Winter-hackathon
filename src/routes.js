6da0288d4b318199de396593c0af8cdfe8c3efc0
Alexandra Zaitsava <ilit_alex@tut.by>
2020-12-23 21:28:52 +0300

    [SH-004]

--- src/routes.js
+++ src/routes.js
@@ -1,43 +1,36 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import AuthPage from "./pages/AuthPage/AuthPage";
-import MainPage from "./components/MainPage/MainPage";
-import Profile from "./components/Profile/Profile";
+import MainPage from "./pages/MainPage/MainPage";
 
 export const useRoutes = (isAuthenticated) => {
-  if (isAuthenticated) {
-    return (
-      <Switch>
-        {/* <Route path="/product/:id" component={ProductPage} />
+    if (isAuthenticated) {
+        return (
+            <Switch>
+                {/* <Route path="/product/:id" component={ProductPage} />
         <Route path="/user/:id">
           <UserPage />
         </Route>*/}
-        <Route path="/auth" exact>
-          <AuthPage />
-        </Route>
-        <Route path="/" exact>
-          <MainPage />
-        </Route>
-          <Route path="/profile" exact>
-              <Profile />
-          </Route>
-        <Redirect to="/" />
-      </Switch>
-    );
-  }
+                <Route path="/auth" exact>
+                    <AuthPage />
+                </Route>
+                <Route path="/" exact>
+                    <MainPage />
+                </Route>
+                <Redirect to="/" />
+            </Switch>
+        );
+    }
 
-  return (
-    <Switch>
-      <Route path="/auth" exact>
-        <AuthPage />
-      </Route>
-      <Route path="/" exact>
-        <MainPage />
-      </Route>
-        <Route path="/profile" exact>
-            <Profile />
-        </Route>
-      <Redirect to="/" />
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route path="/auth" exact>
+                <AuthPage />
+            </Route>
+            <Route path="/" exact>
+                <MainPage />
+            </Route>
+            <Redirect to="/" />
+        </Switch>
+    );
 };
